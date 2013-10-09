23c0e48c99cc396a05c9a1fdbe0a6d28328d2282
phewitt2 <phewitt2@binghamton.edu>
2013-10-09 02:36:37 -0400

    Update Driver.java
    Added required driver code in order to satisfy program requirements.  This is the template for a simple builder pattern problem.  It's commented out because it obviously won't compile yet, and I wrote a lot of notes.

--- src/studentOrientation/driver/Driver.java
+++ src/studentOrientation/driver/Driver.java
@@ -9,5 +9,30 @@ public class Driver {
 	public static void main(String[] args) {
 		Money testMoney = new USMoney(24, 56);
 		System.out.println(testMoney);
+		
+		/*Here is the template for the driver code of the builder pattern.
+		  I believe the orientationSchedule should be a data member in student.
+		  We will need to work on the class hierarchy.
+		  I think driver is a good place to start because it should be independent
+		  of the implementation of each class.
+		
+		Student myStudent = new Student();
+		
+		BookstoreChoice myBookstoreChoice = new MandosBookstore();
+		DormChoice myDormChoice = new OnlineDorm();
+		CourseRegChoice myCourseRegChoice = new RegistrarOfficeCourseReg();
+		TourChoice myTourChoice = new FootTour();
+		
+		**Note: instantiation of these concrete objects is what makes this builder pattern**
+		** Pieces are assembled in any order by the client, then given to the builder to  **
+		** construct the complex object.						  **
+		
+		AbstractBuilder orientationSchedule = new OrientationScheduleBuilder();
+		orientationSchedule.construct(myBookstoreChoice, myDormChoice, 
+						myCourseRegChoice, myTourChoice);
+						
+		myStudent.setOrientationSchedule(orientationSchedule.getSchedule());
+		myStudent.displayResults(); //display results will call methods to obtain cost, effort, time and print
+		*/
 	}
 }
